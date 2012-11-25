5cd39832b3ee23b5b3e873e137268285e685c2ae
vcrazy <vcrazy@abv.bg>
2012-11-25 10:52:46 +0200

    jquery added

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var port = 3000,
 	express = require('express'),
 	blog = require('./routes/blog'),
-	app = express();
+	app = express(),
+	http = require('http'),
+    fs = require('fs');
 
 // REST
-// 
+
 // List all posts
-app.get('/posts', blog.getAllPosts);
+app.get('/posts', function(req, res){
+	blog.getAllPosts(req, res);
+});
 
 // List one post with its comments
 app.get('/posts/:id', blog.getOnePost);
@@ -22,6 +26,24 @@ app.delete('/posts/:id', blog.deletePost);
 
 // Comment post
 app.post('/comments/:id', blog.createComment);
+
+// Get /
+app.get('/', function(req, res){
+	fs.readFile('./index.html', function(err, html){
+		res.writeHeader(200, {"Content-Type": "text/html"});
+		res.write(html);
+		res.end();
+	});
+});
+
+// Get /scripts/:src
+app.get('/scripts/:src', function(req, res){
+	fs.readFile('./' + req.params.src, function(err, html){
+		res.writeHeader(200, {"Content-Type": "text/javascript"});
+		res.write(html);
+		res.end();
+	});
+});
 
 // END OF REST
 
