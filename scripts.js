e70a9c1be51efc4be3aeca73f7578cbb3986a546
vcrazy <vcrazy@abv.bg>
2012-11-25 11:01:30 +0200

    first get /posts version

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-	console.log('ready');
+	$.get('/posts', {}, function(posts){
+		setPosts(posts);
+	});
+
+	function setPosts(posts){
+		$('#posts').empty();
+
+		$.each(posts, function(index, post){
+			$('#posts').append('<div class="post">' + post.text + '<div class="comments"></div></div>');
+		});
+	}
 });
